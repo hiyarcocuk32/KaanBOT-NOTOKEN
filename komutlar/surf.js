87bc90d4321e571f079222571417cad9e0353bba
hiyarcocuk32 <43614317+hiyarcocuk32@users.noreply.github.com>
2018-10-18 16:36:39 +0300

    Update surf.js

--- komutlar/surf.js
+++ komutlar/surf.js
@@ -7,7 +7,7 @@ exports.run = (client, message, params) => {
     .setColor(0xFF0000)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .addField('**Eðlence Komutlarý Özel Mesajlarda Kullanýlamaz!**')
+    .addField('**EÃ°lence KomutlarÃ½ Ã–zel Mesajlarda KullanÃ½lamaz!**')
     return message.author.sendEmbed(ozelmesajuyari); }
     if (message.channel.type !== 'dm') {
       const sunucubilgi = new Discord.RichEmbed()
@@ -15,7 +15,7 @@ exports.run = (client, message, params) => {
     .setColor(3447003)
     .setTimestamp()
     .setDescription('')
-		.setImage(`https://www.google.com.tr/url?sa=i&source=images&cd=&ved=2ahUKEwjVhLPpiZDeAhWR_qQKHfpUAAMQjRx6BAgBEAU&url=https%3A%2F%2Fgiphy.com%2Fgifs%2Fsurf-surfing-gopro-3orif4tsbdtVDGxR72&psig=AOvVaw0JZPfU4qvtzBZopHRiOpRO&ust=1539955007276474`)
+		.setImage(`https://www.youtube.com/watch?v=shrlM5ZjdiA`)
     return message.channel.sendEmbed(sunucubilgi);
     }
 };
@@ -29,6 +29,6 @@ exports.conf = {
 
 exports.help = {
   name: 'surf',
-  description: 'Surf Yaparsýnýz.',
+  description: 'Surf YaparsÃ½nÃ½z.',
   usage: 'surf'
 };
