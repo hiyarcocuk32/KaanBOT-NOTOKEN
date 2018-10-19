632c35f1598eaab8f6366c55ff93fc4741b4d304
hiyarcocuk32 <43614317+hiyarcocuk32@users.noreply.github.com>
2018-10-19 16:21:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,7 +90,8 @@ client.on('message', msg => {
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'discord.gg') {
-    msg.delete(30)  
+    msg.delete(30) 
+    client.channels.get(502152951523966977)	  
     msg.reply('**Reklam Yapmaya Devam Edersen Banlanacaksın!!**');
   }
 });
